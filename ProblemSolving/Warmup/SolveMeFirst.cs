48e8ea1dd5a5fe48ef8cc22d5c65018ef10aabee
Emre-Gelen <emregeln@gmail.com>
2022-01-26 13:09:50 +0300

    Added first solution.

---
+++ ProblemSolving/Warmup/SolveMeFirst.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class SolveMeFirst
+{
+    static int solveMeFirst(int a, int b)
+    {
+        return a + b;
+    }
+    static void Main(string[] args)
+    {
+        int val1 = Convert.ToInt32(Console.ReadLine());
+        int val2 = Convert.ToInt32(Console.ReadLine());
+        int sum = solveMeFirst(val1, val2);
+        Console.WriteLine(sum);
+    }
+}
