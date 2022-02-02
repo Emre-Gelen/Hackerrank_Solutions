d95a7bd4e949737c2ddefc45d9272c4c3b39336a
Emre-Gelen <emregeln@gmail.com>
2022-02-02 22:46:24 +0300

    Create MiniMaxSum.cs

---
+++ ProblemSolving/Warmup/MiniMaxSum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    static void miniMaxSum(List<int> arr)
+    {
+        long min = arr[0], max = arr[0], sum = 0;
+        foreach (int item in arr)
+        {
+            sum += item;
+            if (item > max)
+            {
+                max = item;
+            }
+            if (item < min)
+            {
+                min = item;
+            }
+        }
+        Console.WriteLine((sum - max) + " " + (sum - min));
+    }
+
+    static void Main(string[] args)
+    {
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+        miniMaxSum(arr);
+    }
+}
