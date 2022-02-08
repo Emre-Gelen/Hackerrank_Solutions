db49753e11447149635512c61ce48d281cc1f3d0
Emre-Gelen <emregeln@gmail.com>
2022-02-08 23:33:14 +0300

    Added new solutions in strings and added libraries.

---
+++ ProblemSolving/Strings/HackerrankInString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    static string hackerrankInString(string s)
+    {
+        string hackerrank = "hackerrank";
+        int letterCount = 0;
+
+        for (int i = 0; i < s.Length && letterCount < hackerrank.Count(); i++)
+        {
+            if (s[i] == hackerrank[letterCount])
+                letterCount++;
+        }
+        return letterCount == hackerrank.Count() ? "YES" : "NO";
+    }
+    static void Main(string[] args)
+    {
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s = Console.ReadLine();
+            string result = hackerrankInString(s);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ProblemSolving/Strings/MarsExploration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    static int marsExploration(string s)
+    {
+        int differentCount = 0;
+        string SOS = "SOS";
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (SOS[i % 3] != s[i])
+            {
+                differentCount++;
+            }
+        }
+        return differentCount;
+    }
+
+    static void Main(string[] args)
+    {
+        string s = Console.ReadLine();
+
+        int result = marsExploration(s);
+
+        Console.WriteLine(result);
+    }
+}
