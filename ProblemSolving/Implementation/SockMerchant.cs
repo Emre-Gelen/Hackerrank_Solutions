ee39a7b3ee004f6f586cb4b9d240751c4c9ef02d
Emre-Gelen <emregeln@gmail.com>
2022-02-12 20:41:13 +0300

    Create SockMerchant.cs

---
+++ ProblemSolving/Implementation/SockMerchant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    static int sockMerchant(int n, List<int> ar)
+    {
+        int result = 0;
+        ar.Sort();
+
+        for (int i = 0; i < ar.Count - 1;)
+        {
+            if (ar[i] == ar[i + 1])
+            {
+                result++;
+                i += 2;
+                continue;
+            }
+            i++;
+        }
+        return result;
+    }
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = sockMerchant(n, ar);
+
+        Console.WriteLine(result);
+    }
+}
